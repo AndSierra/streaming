5f0eed006d608a4d2c8d7de1662e9c85634542c6
PaulaAndSierra <psierra@fagorelectronica.com.co>
2023-10-04 18:14:58 -0500

    cambio vista tabla

--- resources/views/qrcodes/table.blade.php
+++ resources/views/qrcodes/table.blade.php
@@ -24,9 +24,9 @@
                     <td>{{ $qrcode->product_name }}</td>
                     <td>{{ $qrcode->product_url }}</td>
                     <td>{{ $qrcode->callback_url }}</td>
-                    <td><img width="30%" height="30%" src = "{{ $qrcode->qrcode_path }}"></td>
+                    <td><img width="100%" height="100%" src = "{{ $qrcode->qrcode_path }}"></td>
                     <td>{{ $qrcode->amount }}</td>
-                    <td><img width="10%" height="10%" src = "{{ $qrcode->product_url_image_path }}"> </td>
+                    <td><img width="100%" height="100%" src = "{{ $qrcode->product_url_image_path }}"> </td>
                     <td  style="width: 120px">
                         {!! Form::open(['route' => ['qrcodes.destroy', $qrcode->id], 'method' => 'delete']) !!}
                         <div class='btn-group'>
